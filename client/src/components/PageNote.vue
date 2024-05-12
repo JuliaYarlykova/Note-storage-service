ad6058ef06ea90e819420001554a5868a737c1f9
ChiVika <147921578+ChiVika@users.noreply.github.com>
2024-05-12 23:56:04 +0700

    radio button

--- client/src/components/PageNote.vue
+++ client/src/components/PageNote.vue
@@ -1,22 +1,28 @@
 <template>
   <main class="pageNotes">
     <div class="pageNotes__main-container">
+      <p class="page-folder__text" v-show="show">Вы не вошли в систему</p>
       <div class="pageNotes__panel">
-        <div class="pageNotes__color-picker">
-          <label for="color-black">
-            <input type="radio" id="color-black" name="color" value="black" checked>
+        <div class="pageNotes__title-panel">Цвет текста</div>
+        <div class="pageNotes__color-picker radio">
+          <label for="color-black" class="radio__label">
+            <input type="radio" class="radio__input" id="color-black" name="color" value="black" checked>
+            <span class="radio__mark"></span>
             <span class="radio__name">Черный</span>
           </label>
-          <label for="color-black">
-            <input type="radio" id="color-black" name="color" value="red" checked>
+          <label for="color-red" class="radio__label">
+            <input type="radio" class="radio__input" id="color-red" name="color" value="red">
+            <span class="radio__mark"></span>
             <span class="radio__name">Красный</span>
           </label>
-          <label for="color-black">
-            <input type="radio" id="color-black" name="color" value="blue" checked>
+          <label for="color-blue" class="radio__label">
+            <input type="radio" class="radio__input" id="color-blue" name="color" value="blue">
+            <span class="radio__mark"></span>
             <span class="radio__name">Синий</span>
           </label>
-          <label for="color-black">
-            <input type="radio" id="color-black" name="color" value="green" checked>
+          <label for="color-green" class="radio__label">
+            <input type="radio" class="radio__input" id="color-green" name="color" value="green">
+            <span class="radio__mark"></span>
             <span class="radio__name">Зеленый</span>
           </label>
           
@@ -34,7 +40,6 @@
       </div>
     </div>
       
-    
   </main>
 
 
@@ -47,7 +52,7 @@
   export default {
   name: 'PageNote',
   components: {
-    CardNote
+    CardNote,
   },
   mounted() {
     const colorInputs = document.querySelectorAll('.pageNotes__color-picker input[type="radio"]');
@@ -60,8 +65,6 @@
     });
   }
 }
-  
-
 
 </script>
 
@@ -79,7 +82,7 @@
     &__panel{
       width: 256px;
       height: 754px;
-      background-color: #8CBEC8;
+      background-color: @dark_blue;
     }
     &__container{
       display: flex;
@@ -107,11 +110,11 @@
       display: block;
       font-size: 30px;
       margin-right: 30px;
-      color: #8CBEC8;
+      color: @dark_blue;
     }
     &__input-title{
       max-width: 400px;
-      border-bottom: 2px solid #8CBEC8;
+      border-bottom: 2px solid @dark_blue;
       border-right:none ;
       border-top: none;
       border-left: none;
@@ -119,8 +122,8 @@
     }
     &__textarea{
       width: 100%;
-      border: 2px solid #8CBEC8;
-      outline: #8CBEC8;
+      border: 2px solid @dark_blue;
+      outline: @dark_blue;
       min-height: 300px;
       font-size: 20px;
     }
@@ -131,7 +134,7 @@
       display: inline-block;
       box-sizing: border-box;
       padding: 5px 40px;
-      background-color: #8CBEC8;
+      background-color: @dark_blue;
       outline: none;
       font-size: 16px;
       color: #ffffff;
@@ -148,7 +151,57 @@
     &__color-picker{
       display: flex;
       flex-direction: column;
+      gap: 15px;
     }
-
+    &__title-panel{
+      font-size: 22px;
+      color: @black;
+      margin-bottom: 15px;
+    }
   }
+
+  .radio{
+    display: flex;
+    flex-direction: column;
+    &__name{
+        margin: 15px;
+        font-size: 20px;
+      }
+      &__input{
+        .is-hidden;
+        &:checked{
+            &~.radio__mark{
+                background-color: @black;
+            }
+        }
+      }
+      &__mark{
+        box-sizing: border-box;
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 20px;
+        height: 20px;
+        border-radius: 50px;
+        border: 2px solid @main;
+        padding: 3px;
+        background-clip: content-box;
+        background-color: transparent;
+        transition: background-color 0.2s;
+
+    }
+      &__label{
+        display: block;
+        margin-bottom: 15px;
+        box-sizing: border-box;
+        padding-left: 23px;
+        position: relative;
+        min-height: 23px;
+        cursor: pointer;
+        &:last-child{
+            margin-bottom: 0;
+        }
+      }
+}
+    
 </style>
