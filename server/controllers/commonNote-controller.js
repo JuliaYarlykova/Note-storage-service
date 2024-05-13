8c96145ad8902283ae88c8ee6248792ebd199fd0
JuliaYarlykova <yarlykova2506@gmail.com>
2024-05-13 09:50:20 +0700

    zczxc

--- server/controllers/commonNote-controller.js
+++ server/controllers/commonNote-controller.js
@@ -2,6 +2,7 @@ const { request, response } = require("express");
 const CommonNote = require("../models/CommonNote");
 const Note = require('../models/Note')
 const User = require('../models/User')
+const File =  require('../models/File')
 const Folder = require('../models/Folder')
 
 const getCommonNote = (request, response) => {
@@ -30,8 +31,33 @@ const postCommonNote = async (request,response)=>{
     response.status(400).json({ message: "postCommonNote error" });
   }
 }
+const getOneNote = async (request, response) => {
+  const title = request.params.title;
+  const obj = [];
+  const note = await CommonNote.findOne({ where: { title:title } });
+  console.log(note.id)
+  await File.findAll({ where: { idnote: note.id } })
+    .then((notes) => {
+      for (const note of notes) {
+        const encode = (data) => {
+          let buf = Buffer.from(data);
+          let base64 = buf.toString("base64");
+          return base64;
+        };
+        const html = `data:${note.type};base64,${encode(note.data)}`;
+        obj.push(html);
+      }
+    })
+    .catch((error) => {
+      console.error("Ошибка получения  конспекта:", error);
+      response.status(500).send("Ошибка получения конспекта");
+    });
+    console.log(obj)
+  response.status(200).send({ note, obj });
+};
 
 module.exports = {
   getCommonNote,
-  postCommonNote
+  postCommonNote,
+  getOneNote
 };
--- server/routes/commonNote-router.js
+++ server/routes/commonNote-router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
-const { getCommonNote, postCommonNote } = require("../controllers/commonNote-controller");
+const { getCommonNote, postCommonNote, getOneNote } = require("../controllers/commonNote-controller");
 
 const router = express.Router();
 
 router.get("/api/commonnote", getCommonNote);
 router.post('/api/postcommonnote', postCommonNote)
+router.get('/api/commonnote/:title', getOneNote)
 
 module.exports = router;
