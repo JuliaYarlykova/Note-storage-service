35c4fbbecd6698c8b86ff7ccf9b784c9230140a8
JuliaYarlykova <yarlykova2506@gmail.com>
2024-05-26 09:51:31 +0700

    editFeatures

---
+++ client/src/components/pp/EditNotePP.vue
@@ -0,0 +1,21 @@
+<template>
+	<transition>
+		<div class="pp-note">
+			<div class="pp-note__container">
+				<h1>Редактор конспекта</h1>
+				<button class="pp-note__btn-close" @click="close">
+					<SvgX class="pp-note__svg-cls" />
+				</button>
+				<div class="pp-note__form form">
+					<input
+						type="text"
+						class="pp-note__input"
+						placeholder="название"
+						v-model.trim="inputData"
+					/>
+				</div>
+				<button class="pp-note__btn" @click="edit">Edit</button>
+			</div>
+		</div>
+	</transition>
+</template>
--- server/controllers/folder-controller.js
+++ server/controllers/folder-controller.js
@@ -1,80 +1,89 @@
-const { response, request } = require("express");
-const Folder = require("../models/Folder");
-const { where } = require("sequelize");
+const { response, request } = require('express')
+const Folder = require('../models/Folder')
+const { where } = require('sequelize')
 
 const getFolder = (request, response) => {
-  const id = request.params.id;
-   Folder.findAll({ where: { idUser: id } })
-    .then((folder) => {
-      response.status(200).send(folder);
-    })
-    .catch((error) => {
-      console.error("Ошибка получения списка папок:", error);
-      response.status(500).send("Ошибка получения списка папок");
-    });
-};
+	const id = request.params.id
+	Folder.findAll({ where: { idUser: id } })
+		.then(folder => {
+			response.status(200).send(folder)
+		})
+		.catch(error => {
+			console.error('Ошибка получения списка папок:', error)
+			response.status(500).send('Ошибка получения списка папок')
+		})
+}
 
-const postFolder = (request, response)=>{
-  try{
-    const {title, idUser} = request.body
-    const folder = Folder.create({
-      title: title,
-      idUser: idUser
-    })
-    response.status(200).json({folder})
-  } catch (e){
-    console.log(e)
-    response.status(400).json({message: 'creating folder error'})
-  }
+const postFolder = (request, response) => {
+	try {
+		const { title, idUser } = request.body
+		const folder = Folder.create({
+			title: title,
+			idUser: idUser,
+		})
+		response.status(200).json({ folder })
+	} catch (e) {
+		console.log(e)
+		response.status(400).json({ message: 'creating folder error' })
+	}
 }
 
 const deleteFolder = (request, response) => {
-  const {title, idUser} = request.body
-  Folder.findOne({ where: {title: title, idUser: idUser}}).then((folder)=>{
-  if (!folder) {
-    response.status(404).send("папка не найдена");
-  } else {
-    folder.destroy().then(() => {
-      response.send("пакпа успешно удалена");
-    }).catch(error => {
-      console.error('Ошибка удаления папки:', error);
-      response.status(500).send("Ошибка удаления папки");
-    });
-  }
-}).catch(error => {
-  console.error('Ошибка поиска папки:', error);
-  response.status(500).send("Ошибка удаления папки");
-})
+	console.log(request.body)
+	const { title, idUser } = request.body
+	console.log(title)
+	Folder.findOne({ where: { title: title, idUser: idUser } })
+		.then(folder => {
+			if (!folder) {
+				response.status(404).send('папка не найдена')
+			} else {
+				folder
+					.destroy()
+					.then(() => {
+						response.send('пакпа успешно удалена')
+					})
+					.catch(error => {
+						console.error('Ошибка удаления папки:', error)
+						response.status(500).send('Ошибка удаления папки')
+					})
+			}
+		})
+		.catch(error => {
+			console.error('Ошибка поиска папки:', error)
+			response.status(500).send('Ошибка удаления папки')
+		})
 }
 
-const patchFolder = (request, response) =>{
-  const {titleold, title, idUser} = request.body
-
-  Folder.findOne({where: {title:titleold, idUser:idUser}}).then((folder)=>{
-    if (!folder) {
-      return response.status(404).send("папка не найдена");
-    }
-    folder.update({
-      title:title,
-      idUser:idUser
-    }).then(updatedFolder => {
-      response.send(updatedFolder);
-    }).catch(error => {
-      console.error('Ошибка обновления папки:', error);
-      response.status(500).send("Ошибка обновления папки");
-    });
-  }).catch(error => {
-    console.error('Ошибка поиска папки для обновления:', error);
-    response.status(500).send("Ошибка обновления папки");
-  })
-
+const patchFolder = (request, response) => {
+	const { titleold, title, idUser } = request.body
+	console.log(request.body)
+	Folder.findOne({ where: { title: titleold, idUser: idUser } })
+		.then(folder => {
+			if (!folder) {
+				return response.status(404).send('папка не найдена')
+			}
+			folder
+				.update({
+					title: title,
+					idUser: idUser,
+				})
+				.then(updatedFolder => {
+					response.send(updatedFolder)
+				})
+				.catch(error => {
+					console.error('Ошибка обновления папки:', error)
+					response.status(500).send('Ошибка обновления папки')
+				})
+		})
+		.catch(error => {
+			console.error('Ошибка поиска папки для обновления:', error)
+			response.status(500).send('Ошибка обновления папки')
+		})
 }
-
-
 
 module.exports = {
-  getFolder,
-  postFolder,
-  deleteFolder,
-  patchFolder
-};
+	getFolder,
+	postFolder,
+	deleteFolder,
+	patchFolder,
+}
