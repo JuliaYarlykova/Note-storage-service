c3ad390ca7d0332142a4cd2e526c903ee38996ed
JuliaYarlykova <yarlykova2506@gmail.com>
2024-05-27 09:23:21 +0700

    отредактировала редактирование

--- server/models/Note.js
+++ server/models/Note.js
@@ -1,15 +1,16 @@
 const sequelize = require('./sequelize')
 const Sequelize = require('sequelize')
 
-const Note = sequelize.define('note',{
-    id:{
-        type: Sequelize.STRING,
-      primaryKey: true,
-      allowNull: false,
-    },
-    title: Sequelize.STRING,
-    idFolder: Sequelize.INTEGER
-}    
-);
+const Note = sequelize.define('note', {
+	id: {
+		type: Sequelize.STRING,
+		primaryKey: true,
+		allowNull: false,
+	},
+	title: Sequelize.STRING,
+	idFolder: Sequelize.INTEGER,
+	text: Sequelize.STRING,
+	colorText: Sequelize.STRING,
+})
 
 module.exports = Note
